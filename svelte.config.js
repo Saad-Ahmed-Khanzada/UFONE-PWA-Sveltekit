87058e33de403f7102ea04078298dffa8496960a
Saad-Ahmed-Khanzada <121751553+Saad-Ahmed-Khanzada@users.noreply.github.com>
2024-11-18 20:55:16 +0500

    svelte file redited

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
-import vercel from '@sveltejs/adapter-vercel'
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-kit: {
-    adapter: vercel()
-}
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  onwarn: (warning, handler) => {
+    if (warning.code.startsWith("a11y-")) return;
+    handler(warning);
+  },
+
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    // adapter: adapter()
+    adapter: adapter({ out: "build" }),
+    
+  },
 };
 
 export default config;
